ed26b92c09cbb35d2af652242ced8a0f49923190
WaddleGee <Georgeshapka@gmail.com>
2025-12-08 14:10:56 -0500

    started ui. student create table is almost done

---
+++ PROG2111_FinalPhase5/PROG2111_FinalPhase5/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PROG2111_FinalPhase5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly string ComboBoxString = "System.Windows.Controls.ComboBoxItem: ";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// makes all table editors invisable
+        /// </summary>
+        private void SetAllTablesInvisible()
+        {
+            createStudentGrid.Visibility = Visibility.Hidden;
+        }
+    }
+}
---
+++ PROG2111_FinalPhase5/PROG2111_FinalPhase5/MainWindow_CreateTab.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PROG2111_FinalPhase5
+{
+    /*
+	 * FILE : MainWindow_CreateTab.cs
+	 * PROJECT : $safeprojectname$
+	 * PROGRAMMER : George Shapka
+	 * FIRST VERSION : 12/8/2025 2:06:16 PM
+	 */
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private void createComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selection = createComboBox.SelectedItem.ToString();
+            selection = selection.Remove(0, ComboBoxString.Length);
+            //MessageBox.Show(selection);
+
+            switch (selection)
+            {
+                case "Student Table":
+                    createStudentGrid.Visibility = Visibility.Visible;
+                    break;
+            }
+        }
+    }
+}
