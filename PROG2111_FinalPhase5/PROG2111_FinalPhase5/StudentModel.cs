d89dcff54eb2dfc9d118c4d0c4870dbb2e443a1e
WaddleGee <Georgeshapka@gmail.com>
2025-12-09 01:17:05 -0500

    Reapply "added course"
    This reverts commit 1d05d8d6bb29a445e0b8482915296a7702895b40.

---
+++ PROG2111_FinalPhase5/PROG2111_FinalPhase5/StudentModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PROG2111_FinalPhase5
+{
+    /*
+     * FILE : StudentModel.cs
+     * PROJECT : PROG2111_FinalPhase5
+     * PROGRAMMER : Tuan Thanh Nguyen
+     * FIRST VERSION : 12/08/2025
+     */
+    internal class StudentModel
+    {
+        public int StudentId { get; set; }          // studentID (PK)
+        public int ProgramId { get; set; }          // programID (FK)
+        public string FirstName { get; set; }       // firstName
+        public string LastName { get; set; }        // lastName
+        public string EmailAddress { get; set; }    // emailAddress
+        public DateTime? DateOfBirth { get; set; }  // dateOfBirth (nullable)
+        public DateTime DateEnrolled { get; set; }  // dateEnrolled
+    }
+}
