4e482dcef956e67e04dd0bf28bed4db518e89b5b
WaddleGee <Georgeshapka@gmail.com>
2025-12-09 01:18:02 -0500

    Reapply "added course table"
    This reverts commit a8c1ad2219c5a59199b91309f03a5dde4e95f206.

---
+++ PROG2111_FinalPhase5/PROG2111_FinalPhase5/CourseTable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG2111_FinalPhase5
+{
+    /*
+	 * FILE : CourseTable.cs
+	 * PROJECT : $safeprojectname$
+	 * PROGRAMMER : George Shapka
+	 * FIRST VERSION : 12/9/2025 12:22:38 AM
+	 */
+    internal class CourseTable
+    {
+        public CourseTable()
+        {
+            DataColumn courseId = new DataColumn("courseId", typeof(int));
+            CourseDataTable.Columns.Add(courseId);
+            CourseDataTable.PrimaryKey = new DataColumn[] { courseId };
+            CourseDataTable.Columns.Add("courseTitle", typeof(string));
+            CourseDataTable.Columns.Add("courseDescription", typeof(string));
+            CourseDataTable.Columns.Add("courseHours", typeof(int));
+        }
+
+
+        public DataTable CourseDataTable = new DataTable("CourseTable");
+        public static List<int>CourseIds = new List<int>();
+    }//end of CourseTable
+
+}//end of PROG2111_FinalPhase5
